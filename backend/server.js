46391291782267269341002cdb56f44e6180571b
Pazflow <pazflow21@gmail.com>
2025-05-18 09:32:48 +0200

    edited server.js to use commonjs because of warnings

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import axios from "axios";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config();
 
